2b98123cd625c2221db7326e8e68d594a771eafb
Jake Varley <jakevarley@gmail.com>
2017-02-09 17:38:47 -0500

    will now run tests out of ci

--- ci/script.sh
+++ ci/script.sh
@@ -1,5 +1,16 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$COMPILER" = "CLANG" ]; then exit; fi
 
+git clone git@github.com:google/googletest.git
+cd googletest
+git checkout release-1.8.0
+mkdir build && cd build 
+cmake ..
+make -j4
+sudo make install
+
+cd ..
+cd ..
+
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE 
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE  -DBUILD_TESTS:BOOL=true
 make -j4 
