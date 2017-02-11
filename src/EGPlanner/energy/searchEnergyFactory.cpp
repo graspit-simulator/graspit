851306f4d05631b54c96f200020d8422260f6d97
jvarley <jakevarley@gmail.com>
2017-02-11 12:45:46 -0500

    fixing author info

--- src/EGPlanner/energy/searchEnergyFactory.cpp
+++ src/EGPlanner/energy/searchEnergyFactory.cpp
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU General Public License
 // along with GraspIt!.  If not, see <http://www.gnu.org/licenses/>.
 //
-// Author(s): Matei T. Ciocarlie
+// Author(s): Iretiayo Akinola, Jake Varley
 //
 // $Id: searchEnergy.cpp,v 1.42 2009/09/13 19:57:38 hao Exp $
 //
